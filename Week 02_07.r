3357c5b68713663d8cb80d169b18c38daca750a3
MadhawaHulangamuwa <mkb.hulangamuwa.gmail.com>
2023-03-04 09:40:58 +0530

    Innitial commit

--- Week 02_07.r
+++ Week 02_07.r
@@ -1,37 +1,33 @@
 library(datasets)
-data("iris")
-View(iris)
+library(ggplot2)
+library(GGally)
 
+
+data("iris")
+head(iris,10)
+View(iris)
 unique(iris$Species)
 
-library(datasets)
-# Load Data
 data(mtcars)
-# View first 5 rows
 head(mtcars, 5)
 
-#load ggplot package
-library(ggplot2)
-# create a scatterplot of displacement (disp) and miles per gallon (mpg)
-ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()
 
 
-# Add a title
 ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon")
+ggplot(aes(x=Sepal.Length,y=Species,),data=iris) + geom_point() + ggtitle("Sepal length vs Species")
 
 # change axis name
 ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()+ggtitle("displacement vs miles per gallon") + labs(x = "Displacement", y = "Miles per Gallon")
 
 
 
-
-# Use the following to create a boxplot of the the distribution of mpg for the individual Engine types vs Engine (0 = V-shaped, 1 = straight)
-# To do this you have to make vs a string or factor.
-
 #make vs a factor
 mtcars$vs <- as.factor(mtcars$vs)
+iris$Species <- as.factor(iris$Species)
+
 # create boxplot of the distribution for v-shaped and straight Engine
 ggplot(aes(x=vs, y=mpg), data = mtcars) + geom_boxplot()
+ggplot(aes(x=Species, y=Petal.Width), data = iris) + geom_boxplot()
 
 # Add color to the boxplots to help differentiate:
 ggplot(aes(x=vs, y=mpg, fill = vs), data = mtcars) + geom_boxplot(alpha=0.3) + theme(legend.position="none") 
@@ -41,7 +37,5 @@ ggplot(aes(x=mpg, y=vs, fill = vs), data = mtcars) + geom_boxplot(alpha=0.3) + t
 # Finally, let us create the histogram of weight wt.
 ggplot(aes(x=wt),data=mtcars) + geom_histogram(binwidth=0.5)
 
-
-library(GGally)
 ggpairs(iris, mapping=ggplot2::aes(colour = Species))
 
